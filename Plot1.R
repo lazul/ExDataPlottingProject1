7a144f2ae410451e9d78526c2f3a354d9a5dc5dd
lazul <natidra@gmail.com>
2015-10-09 19:26:07 -0600

    initial

---
+++ Plot1.R
@@ -0,0 +1,20 @@
+
+Plot1 <- function(){
+  
+	## Load dplyr for filtering function
+	library(dplyr)
+	
+  ## Load project data (as long as its in "Working Directory")
+	hpc <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
+  
+  ## Change Date column to a 'Date' Class-type
+	hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+  
+  ## Filter relevant columns for assignment Feb. 1 & 2, 2007 and create a new data Table with result
+	hpcFiltered <- filter(hpc, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+  
+	## use Date Column from filtered Data Table to graph Histogram
+	par(cex = .7)
+	hist(hpcFiltered$Global_active_power, xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red", main = "Global Active Power")
+  
+}
---
+++ Plot3.R
@@ -0,0 +1,31 @@
+
+Plot3 <- function(){
+  
+  ## Load dplyr for filtering function
+  library(dplyr)
+  
+  ## Load project data (as long as its in "Working Directory")
+  hpc <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
+  
+  ## Merge Date field with Time field
+  hpc$DT <- paste(hpc$Date, hpc$Time)
+  
+  ## Change Date column to a 'Date' Class-type
+  hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+  
+  ## Filter relevant columns for assignment Feb. 1 & 2, 2007 and create a new data Table with result
+  hpcFiltered <- filter(hpc, Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+  
+  ## Convert DT (Date/Time) Column to Type class(Date)
+  hpcFiltered$DT <- strptime(hpcFiltered$DT, "%d/%m/%Y %H:%M:%S")
+  
+  ## Plot Graph
+  par(cex = .65, mar = c(4,4,4,4))
+  with(hpcFiltered, plot(DT, Sub_metering_1, type = "n", ylab = "Energy sub metering", xlab = ""))
+  with(hpcFiltered, lines(DT, Sub_metering_1))
+  with(hpcFiltered, lines(DT, Sub_metering_2, col = "red"))
+  with(hpcFiltered, lines(DT, Sub_metering_3, col = "blue"))
+  legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  
+  
+}
